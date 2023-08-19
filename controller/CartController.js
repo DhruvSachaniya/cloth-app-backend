6529d0cceb919369ee2d66c372f7ac2f1b433dd5
Dhruv <sachaniyadhruv@email.com>
2023-08-19 21:50:44 +0530

    complete addtocart function

--- controller/CartController.js
+++ controller/CartController.js
@@ -1,23 +1,40 @@
 const cart = require('../models/Carts');
+const product = require('../models/Products');
 
 exports.addtoCart = async (req, res) => {
     try {   
         const { productid, quntity } = req.body;
+
+        const foundproduct = await product.findById(productid);
+
         const seller = req.user.userId;
+        const productprice = foundproduct.price;
+        
         const usercart = await cart.findOne({seller});
-        if(usercart) { 
+        if(usercart) {
             if (usercart.items.length >= 1) {
-    
-                await usercart.items.push(productid);
-                const udatecart = await usercart.save();
-    
-                res.send(udatecart);
+                let productalreadyInCart = false;
+
+                for (let i = 0; i < usercart.items.length; i++) {
+                    if (usercart.items[i] === productid) {
+                        productalreadyInCart = true;
+                        usercart.quntity += 1;
+                        break;
+                    } 
+                }
+                if(!productalreadyInCart) {
+                    usercart.items.push(productid);
+                    usercart.subtotal += foundproduct.price;
+                }
+                const updatecart = await usercart.save();
+                res.status(200).json(updatecart); 
             } 
         } else {
             const newcart = await new cart({
                 seller: seller,
                 items: productid,
                 quntity: quntity,
+                subtotal: productprice,
                 createdAt: date = new Date()
             })
         
@@ -28,3 +45,18 @@ exports.addtoCart = async (req, res) => {
         res.send(error);
     }
 };
+
+exports.cartinfo = async (req, res) => {
+    try {
+        const seller = req.user.userId;
+        const getcart = await cart.findOne({seller});
+
+        if (getcart) {
+            res.status(200).json(getcart)
+        } else {
+            res.status(500).json({meassage: "cart not found!"});
+        }
+    } catch (error) {
+        res.status(500).json({meassage: "error!"});
+    }
+}
--- router/CartRoutes.js
+++ router/CartRoutes.js
@@ -4,5 +4,6 @@ const CartController = require('../controller/CartController');
 const { auth } = require('../middlewares/auth');
 
 router.post('/cart',auth, CartController.addtoCart);
+router.get('/cart',auth, CartController.cartinfo);
 
 module.exports = router;
