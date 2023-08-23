d6c5996a9c3de689568db19ee54da512fe4d0cb7
Dhruv <sachaniyadhruv@email.com>
2023-08-23 10:50:48 +0530

    complete account routes

--- controller/accountController.js
+++ controller/accountController.js
@@ -1,4 +1,5 @@
 const user = require('../models/User');
+const bcrypt = require('bcrypt');
 
 exports.accountinfo = async (req, res) => {
     try {
@@ -9,20 +10,99 @@ exports.accountinfo = async (req, res) => {
         if (founduser) {
             res.status(200).json(founduser);
         } else {
-            res.status(401).json({meassage: "user not found!"});
+            res.status(401).json({ meassage: "user not found!" });
         }
     } catch (error) {
         res.status(500).json(error);
     }
 }
 
-exports.editaccount = async (req, res) => {
+exports.editusername = async (req, res) => {
     try {
+        const { username } = req.body;
+
+        const seller = req.user.userId;
+
+        const updateuser = await user.findOneAndUpdate(
+            { _id: seller},
+            { $set: {username} },
+            { new: true }
+        );
+
+        if(!updateuser) {
+            return res.status(404).json({ meassage: "user not found!"});
+        }
+        
+        res.status(200).json({ meassage: "username updated successfully", user: updateuser });
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+exports.editemail = async (req, res) => {
+    try {
+        const { email } = req.body;
+
+        const seller = req.body.userId;
+
+        const updateuser = await user.findOneAndUpdate(
+            { _id: seller},
+            { $set: {email} },
+            { new: true }
+        )
+
+        if(!updateuser) {
+            res.status(404).json({ meassage: "user not found!"});
+        }
+
+        res.status(200).json({ meassage: "email updeted sucessfully!", user: updateuser});
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+exports.editpassword = async (req, res) => {
+    try {
+        const { newpassword, oldpassword} = req.body;
+
+        const bcrynewpass = await bcrypt.hash(newpassword, 10);
+
         const seller = req.user.userId;
 
         const founduser = await user.findById(seller);
 
-        //
+        const ispasswordValid = await bcrypt.compare(oldpassword, founduser.password);
+
+        if(ispasswordValid) {
+            const updateuser = await user.findOneAndUpdate(
+                { _id: seller},
+                { $set: {password: bcrynewpass} },
+                {new: true}
+            );
+
+            if(!updateuser) {
+                res.status(404).json({ meassage: "user not found!"});
+            }
+
+            res.status(200).json({ meassage: "password updeted sucessfully", user: updateuser});
+        } else {
+            res.status(404).json({ meassage: "oldpassword is incorrect!"});
+        }
+
+    } catch (error) {
+        res.status(500).json(error);
+    }
+}
+
+exports.deleteaccount = async (req, res) => {
+    try {
+        const { confirm } = req.body;
+        const seller = req.user.userId;
+        if (confirm) {
+            const founduser = await user.findByIdAndDelete(seller);
+            
+            res.status(200).json({ meassage: "your account hase been deleted!", founduser });
+        }
     } catch (error) {
         res.status(500).json(error);
     }
--- router/accountRoutes.js
+++ router/accountRoutes.js
@@ -4,5 +4,8 @@ const accountController = require('../controller/accountController');
 const { auth } = require('../middlewares/auth');
 
 router.get('/account', auth, accountController.accountinfo);
+router.patch('/username', auth, accountController.editusername);
+router.patch('/email', auth, accountController.editemail);
+router.patch('/password', auth, accountController.editpassword);
 
 module.exports = router;
